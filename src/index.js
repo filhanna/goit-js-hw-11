298ad79b05e20191d560a8b949208d6bccd37c15
filhanna <filimonenkohanna@gmail.com>
2023-01-29 11:19:31 +0200

    task completed

--- src/index.js
+++ src/index.js
@@ -3,30 +3,43 @@ import { renderGallery } from "./js/render-gallery";
 import Notiflix from "notiflix";
 const searchForm = document.querySelector("#search-form");
 const gallery = document.querySelector(".gallery");
-const loadMoreBtn = document.querySelector(".btn-load-more");
+const loadMoreBtn = document.querySelector(".load-more");
 let page = 1;
-loadMoreBtn.classList.add("is-hidden");
-
+let query;
+loadMoreBtn.hidden = true;
 searchForm.addEventListener("submit", onSearchForm);
-// loadMoreBtn.addEventListener("click", onLoadMoreBtn);
+loadMoreBtn.addEventListener("click", onLoadMoreBtn);
 
-function onSearchForm(evt) {
+async function onSearchForm(evt) {
   evt.preventDefault();
+  loadMoreBtn.hidden = true;
   gallery.innerHTML = "";
   page = 1;
-  // loadMoreBtn.hidden = false;
+  query = evt.currentTarget[0].value.trim();
   loadMoreBtn.classList.remove("is-hidden");
-  if (evt.currentTarget[0].value.trim() === "") {
+  if (query === "") {
     Notiflix.Notify.failure("Please fill in the search field");
-  }
-  console.log(evt.currentTarget[0].value);
-  fetchImages(evt.currentTarget[0].value).then((data) => {
-    if (data.hits === []) {
+  } else {
+    const images = await fetchImages(query, page);
+    console.log(images);
+    if (images.totalHits === 0) {
       Notiflix.Notify.failure(
         "Sorry, there are no images matching your search query. Please try again."
       );
+    } else {
+      gallery.insertAdjacentHTML("beforeend", renderGallery(images.hits));
+      loadMoreBtn.hidden = false;
     }
-    gallery.insertAdjacentHTML("beforeend", renderGallery(data.hits));
-    return ``;
-  });
+  }
+}
+async function onLoadMoreBtn() {
+  page += 1;
+  const images = await fetchImages(query, page);
+  if (images.hits.length < 40 && images.hits.length !== 0) {
+    loadMoreBtn.hidden = true;
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+  gallery.insertAdjacentHTML("beforeend", renderGallery(images.hits));
 }
